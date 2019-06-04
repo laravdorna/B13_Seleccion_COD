4864726b5cbf6ef9c818be38bb5126d381e64823
laravdorna <lvazquezdorna@danielcastelao.org>
2019-06-04 09:51:09 +0200

    2.Clase Padre

--- src/Seleccion.kt
+++ src/Seleccion.kt
@@ -1,4 +1,8 @@
- open class Seleccion (var id: Int=0,var nome: String?="default value", var apelido:String="default value", var edade: Int=0){
+/*
+* Clase padre Seleccion
+* */
+
+open class Seleccion (var id: Int,var nome: String , var apelido:String="" , var edade: Int ){
     override fun toString(): String {
         return "Plantilla: \n ID :\t$id\n NOME: \t$nome\n APELIDOS: \t$apelido\n EDADE: \t$edade"
     }
